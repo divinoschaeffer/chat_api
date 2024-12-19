d69efc59a6504f41f4b1a845f4c01c7e1177754b
Divino Schaeffer <divino.schaeffer02@gmail.com>
2024-12-19 22:44:51 +0100

    migration: users table

---
+++ migrations/20241219213749_add_user_table.up.sql
@@ -0,0 +1,8 @@
+-- Add up migration script here
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
